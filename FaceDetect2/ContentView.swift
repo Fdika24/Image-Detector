477acebf711b90e2f67442d95887bb9e9b0576b0
Fdika24 <71744284+Fdika24@users.noreply.github.com>
2021-07-29 18:45:51 +0700

    Initial Commit

---
+++ FaceDetect2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceDetect2
+//
+//  Created by Farhandika Zahrir Mufti guenia on 29/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
